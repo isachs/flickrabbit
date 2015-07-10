2ec9ee39ef430ed8ac3ea42bb081d8c35a159493
isachs <isachs@ebay.com>
2015-07-09 17:13:31 -0700

    bump version numbers and update help text

--- app/src/main/java/com/icantdescribe/flickrabbit/HelpFragment.java
+++ app/src/main/java/com/icantdescribe/flickrabbit/HelpFragment.java
@@ -9,9 +9,8 @@ import android.os.Bundle;
 public class HelpFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.help_text)
+        builder.setMessage(R.string.help_text).setTitle(R.string.help_title)
                 .setPositiveButton(R.string.action_ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // nothing to do here
